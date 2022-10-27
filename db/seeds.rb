ef68f2d6c3bcb48132dcca2105ca6276086abb1d
YannGUIOT <guiot_yann@hotmail.fr>
2022-10-27 15:52:49 +0200

    Doc Free

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do 
+  city = City.create!(
+    name: Faker::Address.city
+  )
+end
+
+10.times do 
+  doctor = Doctor.create!(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    zip_code: Faker::Address.zip_code,
+    speciality: ["dentist", "surgeon", "cardiologist", "ophtamologist", "family doctor", "pediatrician", "dermatologist"].sample,
+    city: City.all.sample
+  )
+end
+
+20.times do
+  patient = Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    city: City.all.sample
+  )
+end
+
+30.times do
+  doc = Doctor.all.sample
+  appointment = Appointment.create!(doctor:doc, patient:Patient.all.sample, city:doc.city)
+end
